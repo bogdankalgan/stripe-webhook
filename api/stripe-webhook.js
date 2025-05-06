a9a8546b704e406ad229aeac5db9965b9d6fb647
Bogdan-work <kalganbogdan@gmail.com>
2025-05-06 13:35:04 +0200

    second commit

--- api/stripe-webhook.js
+++ api/stripe-webhook.js
@@ -11,6 +11,8 @@ export const config = {
     }
 }
 
+
+
 export default async function handler(req, res) {
     if(req.method !== 'POST') {
         return res.status(405).end("Method not allowed")
@@ -23,36 +25,39 @@ export default async function handler(req, res) {
     let event;
 
     try {
-        event = stripe.webhooks.constructEvent(buf, sig, endpointSecret)
-    } catch (error) {
-        console.error("Stipe webkook error:", error.message)
-        return res.status(400).send(`Webhook Error: ${error.message}`)
+        event  = stripe.webhooks.constructEvent(buf, sig, endpointSecret)
+    } catch (err) {
+        console.error("Stripe Webhook Error: ", err.message)
+        return res.status(400).end(`Webhook Error: ${err.message}`)
     }
 
     if(event.type === 'checkout.session.completed') {
         const session = event.data.object;
 
-        const email = session.customer_details?.email || ""
-        const name = session.shipping?.name || ""
-        const adress = session.shipping?.address?.line1 || ""
-        const total = session.amount_total
-        const paymentIntent = session.payment_intent
+        const email = session.customer_details?.email || '';
+        const name = session.shipping?.name || ''
+        const phone = session.customer_details?.phone || ''
+        const address = session.shipping?.address?.line1 || ''
+        const total = session.amount_total;
+        const paymentIntent = session.payment_intent;
 
-        const {error} = await supabase.from('orders').insert({
-            set_name: "Набор макарон с индивидуальным дизайном",
+        const {error } = await supabase.from('orders').insert([{
+            set_name: "Набор макарон",
             customer_name: name,
-            delivery_adress: adress,
-            comment: email,
-            amount_total: total,
+            phone,
+            delivery_method: "",
+            deliver_datetime: '',
+            comment:'',
+            delivery_address: address, email,
             payment_intent: paymentIntent,
-            created_at: new Date(session.created * 1000)
-        })
+            created_at: new Date(session.created * 1000),
+        }])
+
         if(error) {
-            console.error("Supabse insert error", error)
+            console.error("Supabase insert error: ", error)
             return res.status(500).send("DB insert failed")
         }
     }
 
     res.status(200).json({received: true})
-
 }
