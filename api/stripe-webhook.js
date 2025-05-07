68ce70bc6915bf8456c1609c4e4f66aa40878e43
Bogdan-work <kalganbogdan@gmail.com>
2025-05-07 15:52:20 +0200

    new whole coe

--- api/stripe-webhook.js
+++ api/stripe-webhook.js
@@ -1,3 +1,4 @@
+/*
 const {buffer} = require('micro')
 const Stripe = require('stripe')
 const {createClient} = require("@supabase/supabase-js")
@@ -67,4 +68,67 @@ module.exports = async function handler(req, res) {
         console.log("Order saved to Supabase")
     }
     res.status(200).json({received: true})
+}*/
+
+
+const {stripe} = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const {buffer} = require('micro')
+const {createClient} = require("@supabase/supabase-js")
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPBASE_KEY)
+
+module.exports = async (req, res) => {
+    if(req.method !== 'POST') {
+        return res.status(405).send('Method not allowed')
+    }
+
+    const sig = req.headers['stripe-signature']
+    let event;
+
+    const buf = await buffer(req)
+
+    try{
+        event = stripe.webhooks.constructEvent(buf, sig, process.env.STRIPE_WEBHOOK_SECRET)
+    } catch(error) {
+        console.error("Webhook signature verification failed. ", error)
+        return res.status(400).send(`Webhook Error: ${error.message}`)
+    }
+
+    if(event.type === 'checkout.session.completed') {
+        const session = event.data.object;
+
+        const adress = session.customer_details?.address
+        const fullAddress = adress ? `${adress.line1 || ""}, ${adress.city || ""}, ${adress.postal_code || ""}`: ''
+
+        const dataToInsert = {
+            set_name: "custom set",
+            costomer_name: session.customer_details?.name || '',
+            phone: session.customer_details?.phone || '',
+            delivery_method: "доставка",
+            delivery_datetime: "",
+            comment: "",
+            payment_method: session.payment_method_types[0] || '',
+            delivery_adress: fullAddress || '',
+        }
+
+        try {
+            const {error} = await supabase.from('orders').insert([dataToInsert])
+
+            if(error) {
+                console.error("Supabase insert error: ", error.message)
+                return res.status(500).send(`Supabase insert failed: ${error.message}`)
+            }
+            return res.status(200).send('Order saved to Supabase')
+        } catch(error) {
+            console.error("Unexpected error: ", error.message)
+            return res.status(500).send(`error: ${error.message}`)
+        }
+    }
+    return res.status(200).send('OK')
+}
+
+module.exports.config = {
+    api: {
+        bodyParser: false
+    }
 }
